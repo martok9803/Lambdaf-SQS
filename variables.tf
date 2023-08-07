05d45b5fccc4ddd9757c622441088b05a0f7fa2e
Martin Svetoslavov Kirov <100728345+martok9803@users.noreply.github.com>
2023-08-07 15:19:50 +0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+ 	description = "aws-region"
+}
+
+variable "lambda_name" {
+	description = "lambdafunctionaf"
+}
+
+variable "bucket_name" {
+	description = "martis3bucket1"
+}
+
+variable "sqs_name" {
+	description = "EventsAF"
+}
